85a09723a6ea6fd3488fccd5e74fca9932d65a38
Runemark <martin@runemark.com>
2015-06-08 17:15:54 -0600

    Adding basic project

---
+++ ChronomancyTests/ChronomancyTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChronomancyTests.swift
+//  ChronomancyTests
+//
+//  Created by Alicia Cicon on 6/8/15.
+//  Copyright (c) 2015 Runemark. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ChronomancyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
